38db32a6e992a17fd4043dded94d0fe83f1439b4
Raed159753 <raed.charrad91@gmail.com>
2022-02-04 22:28:36 +0100

    last

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,10 +9,9 @@ use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
     public function index(){
-        if(Auth::user()->hasRole('user')){
-
+        if((Auth::user()->hasRole('user'))&&(Auth::user()->hasVerifiedEmail())){
             return view('welcome');
-        }elseif(Auth::user()->hasRole('stylist')){
+        }elseif((Auth::user()->hasRole('stylist'))&&(Auth::user()->hasVerifiedEmail())){
             return view('stylistDash');
         }elseif(Auth::user()->hasRole('admin')){
             return view('dashboard');
